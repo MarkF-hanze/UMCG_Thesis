f491083cf24b40971939d0b8e3bc7db7b60a6489
guest17139@student.rug.nl <mark.leendert@gmail.com>
2021-09-27 13:26:44 +0200

    Clean

--- Working_Code/HDDR3.R
+++ Working_Code/HDDR3.R
@@ -6,11 +6,9 @@ setwd("/home/g0017139/UMCG_Thesis/Working_Code/")
 library(HDclassif)
 mydata = read.csv("/home/g0017139/UMCG_Thesis/Working_Code/Results/gene_expression_norm.dat", sep = ' ', header = FALSE, )
 ptm <- proc.time()
-prms <- hddc(mydata, K = 15, model = "ALL", itermax = 200, threshold = c(0.01, 0.1, 0.2))
+prms <- hddc(mydata, K = 3, model = 14, itermax = 10000, threshold =  0.1)
 print(proc.time() - ptm)
 
-df = do.call(rbind, prms$allCriteria)
-write.csv(df, "Results/HDDCGrid3.csv")
 df <- do.call("rbind", lapply(prms$class, as.data.frame)) 
-write.csv(df, file = "Results/HDDCClusters3.csv")
+write.csv(df, file = "Results/Clusters3HDDC.csv")
 
