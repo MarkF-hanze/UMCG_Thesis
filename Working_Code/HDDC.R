b400964623b2a53259b3a92def12160c19963bbe
guest17139@student.rug.nl <mark.leendert@gmail.com>
2021-10-20 14:15:38 +0200

    cleaned

--- Working_Code/HDDC.R
+++ Working_Code/HDDC.R
@@ -1,14 +1,13 @@
 library(HDclassif)
 library(data.table)  
 mydata <- fread("/data/g0017139/GPL570_norm.dat", data.table=FALSE)
+#mydata <- fread("/data/g0017139/gene_expression_norm.dat", data.table=FALSE)
 
-
-prms <- hddc(mydata, K = 10, model = 8, itermax = 200, threshold =  0.01, mc.cores = 2)
+prms <- hddc(mydata, K = 5, model = 12, itermax = 200, threshold =  0.01, mc.cores = 1)
 
 i = 0
 for (m in prms$Q){ 
   df = data.frame(m)
-  print(df)
   path = paste(c('/home/g0017139/UMCG_Thesis/Working_Code/Results/Set2/eigenvec',i, '.csv'), collapse = "")
   write.csv(df, path)
   i = i + 1
